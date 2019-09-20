b01ec3955d5e7d501313d11b287cb3589f59e86d
Giovanni <giovannilauro@gmail.com>
2019-09-20 17:42:09 -0400

    Added - BroadcastChannel Support

--- src/angular-jstore.js
+++ src/angular-jstore.js
@@ -27,16 +27,39 @@
      * */
     jstore.provider('$jstore', function $jstoreProvider() {
 
-        // Default prefix
+        // Default values
         var _prefix = 'jstoreApp-';
+        var _channel = [];
+        var _debug = false;
+        var _tabStatus = false;
 
         // Setter for the prefix
         // e.g. $jstoreProvider.setPrefix('myAppName');
-        this.setPrefix = function(prefix) {
-            if (typeof prefix !== 'string')
+        this.setPrefix = function(value) {
+            if (typeof value !== 'string')
                 throw new TypeError('$jstoreProvider.setPrefix() expects a String.');
 
-            _prefix = prefix;
+            _prefix = value;
+            return this;
+        };
+
+        // Setter to enable debugging
+        // e.g. $jstoreProvider.setDebug(true);
+        this.setDebug = function(value) {
+            if (typeof value !== 'boolean')
+                throw new TypeError('$jstoreProvider.setDebug() expects a Boolean.');
+
+            _debug = value;
+            return this;
+        };
+
+        // Setter to apply inactive state on tabs
+        // e.g. $jstoreProvider.setTabStatus(true);
+        this.setTabStatus = function(value) {
+            if (typeof value !== 'boolean')
+                throw new TypeError('$jstoreProvider.setTabStatus() expects a Boolean.');
+
+            _tabStatus = value;
             return this;
         };
 
@@ -51,7 +74,7 @@
          * angular.module('appExample', ['angular-jstore'])
          *   .controller('ExampleController', ['$jstore', function($jstore) {
          *     // Session name
-         *     var SESSION_NAME = "MySessionName";
+         *     var SESSION_NAME = "MyYourSessionName";
          *     // Add data in session localStorage
          *     $jstore.set(SESSION_NAME, { year: "2017" });
          *     // Recover data from session
@@ -258,6 +281,87 @@
                         if (store.key(i).indexOf(_prefix) === 0) {
                             callback(id, this.get(id));
                         }
+                    }
+                },
+
+                /**
+                 * @ngdoc method
+                 * @name $jstore#fire
+                 *
+                 * @description
+                 * Triggers a command listening to the channel.
+                 */
+                fire: function(obj) {
+                    if (typeof obj !== 'object') {
+                        throw new TypeError('$jstore.fire() expects a Object has obtained a `' + (typeof obj) + '` as a parameter. Try something like: { command: "reload" }');
+                    }
+
+                    if (!obj.command) {
+                        return;
+                    }
+
+                    if (_debug) {
+                        console.log('fired >>', obj.command);
+                    }
+
+                    if (_tabStatus) {
+                        top.document.title = top.document.title.replace(/\(inactive\)/,'');
+                    }
+
+                    localStorage.setItem('cmd', obj.command);
+                    localStorage.removeItem('cmd');
+                },
+
+                /**
+                 * @ngdoc method
+                 * @name $jstore#fired
+                 *
+                 * @description
+                 * Fires the corresponding command listening to the channel.
+                 */
+                fired: function(evt) {
+                    angular.forEach(_channel, function(v, k) {
+                        if (evt.newValue == _channel[k][0]) {
+                            if (_debug) {
+                                console.log(' fire >>', _channel[k][0]);
+                            }
+
+                            if (_tabStatus) {
+                                top.document.title = top.document.title.replace(/\(inactive\)/,'') + ' (inactive)';
+                            }
+
+                            _channel[k][1](evt);
+                        }
+                    });
+                },
+
+                /**
+                 * @ngdoc method
+                 * @name $jstore#watch
+                 *
+                 * @description
+                 * Stores watchers that trigger a command on the channel listener.
+                 */
+                watch: function(cmd, callback) {
+                    if (typeof cmd !== 'string') {
+                        throw new TypeError('$jstore.watch() expects a String but got a `' + (typeof cmd) + '` as a first parameter.');
+                    }
+
+                    if (typeof callback !== 'function') {
+                        throw new TypeError('$jstore.watch() expects a Callback Function with a second argument.');
+                    }
+
+                    if (_debug) {
+                        console.log('watch >>', cmd);
+                    }
+
+                    _channel.push([cmd, callback]);
+
+                    // Verify support for IE < 9
+                    if (window.addEventListener) {
+                        window.addEventListener('storage', this.fired, false);
+                    } else if (window.attachEvent) {
+                        window.attachEvent('onstorage', this.fired);
                     }
                 },
 
